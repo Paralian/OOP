88896abc21c992d181af7eacc22967ce2a588367
Paralian <eyeize@gmail.com>
2018-10-31 20:05:47 +0100

    A1 nochmal verbessert A3 optimisiert (und gleichzeitig noch komplizierter gemacht)

--- Woche3/aufgabeDrei.java
+++ Woche3/aufgabeDrei.java
@@ -11,31 +11,48 @@ public class aufgabeDrei {
         return (lo <= val && val <= hi);    // compare lower bound with value AND compare higher bound with value
     }
     
+    /**
+     * postage() rechnet Portokosten aus gegebenen Maßen des Pakets aus
+     *
+     * @param length Länge des Pakets
+     * @param width  Breite des Pakets
+     * @param height Höhe des Pakets
+     * @param weight Gewicht
+     * @return Preis in Cent
+     */
+    
+    
     static int postage(int length, int width, int height, int weight) {
-        int price = 0;          //  init variable in cent
+        int price = 0;                                                           //  Preis (Cent)
         
-        if (isBetween(length, 140, 235)
-                && isBetween(width, 90, 125)) {
-            if (isBetween(height, 0, 2)
-                    && isBetween(weight, 0, 15)) {
-                price = 45;                                         //  Postkarte
-            } else if (isBetween(height, 0, 5)
-                    && isBetween(weight, 0, 20)) {
-                price = 70;                                         //  Standard
-            }
-        } else if (isBetween(length, 100, 235)
-                && isBetween(width, 70, 125)
-                && isBetween(height, 0, 10)
-                && isBetween(weight, 0, 50)) {
-            price = 85;                                             //  Kompakt
-        } else if (isBetween(length, 100, 353)
-                && isBetween(width, 70, 250)) {
-            if (isBetween(height, 0, 20)
-                    && isBetween(weight, 0, 500)) {
-                price = 145;                                        //  Gross
-            } else if (isBetween(height, 0, 50)
-                    && isBetween(weight, 0, 1000)) {
-                price = 260;                                        //  Maxi
+        //  m[i][const]: unterschiedliche Preisklassen
+        //  m[const][j]: price, weight(lo, hi), height(lo, hi), width(lo, hi), length(lo, hi)
+        final int[][] M = {{45, 0, 2, 0, 15, 90, 125, 140, 235},                       //  Postkarte
+                {70, 0, 5, 0, 20, 90, 125, 140, 235},                            //  Standard
+                {85, 0, 10, 0, 50, 70, 125, 100, 235},                           //  Kompakt
+                {145, 0, 20, 0, 500, 70, 250, 100, 353},                         //  Gross
+                {260, 0, 50, 0, 1000, 70, 250, 100, 353}};                       //  Maxi
+        
+        /*  Parameter des Pakets (Höhe, Gewicht, Breite, Länge), gespeichert als Array */
+        int[] p = {height, weight, width, length};
+        
+        /*  vergleiche Paketabmessungen mit den Vorgaben */
+        int pass = 0;                                                            // interner Zähler von passenden Parametern
+        
+        spalte:
+        for (int i = 0; i < M.length; i++) {                                     // iteriere m[][] Zeilenweise
+            zeile:
+            for (int j = 1; j < M[i].length - 1; j = j + 2) {                    // iteriere m[][] Spaltenweise(2x)
+                if (isBetween(p[((j + 1) / 2) - 1], M[i][j], M[i][j + 1])) {     // verknüpfe Pointer von p[] mit j aus m[][]
+                    pass++;                                                      // und prüfe, ob Betrag P[x] im Intervall M
+                    if (pass >= 4) {                                             // falls ja, inkrementiere PASS und prüfe ob alle Abmessungen gestimmt haben
+                        price = M[i][0];
+                        break spalte;                                            // wenn alles stimmt, nehme Preis
+                    }                                                            // aus der ersten Spalte in M
+                } else {                                                         // sonst beim ersten nehme nächste Zeile aus m[][]
+                    pass = 0;                                                    // und setze PASS zurück
+                    break zeile;
+                }
             }
         }
         
@@ -43,10 +60,11 @@ public class aufgabeDrei {
     }
     
     public static void main(String[] args) {
-        int length = 144;
-        int width = 90;
-        int height = 13;
-        int weight = 420;
+        int length = 141;
+        int width = 92;
+        int height = 31;
+        int weight = 415;
         System.out.println(postage(length, width, height, weight));
+        
     }
 }
--- Woche3/aufgabeEins.java
+++ Woche3/aufgabeEins.java
@@ -1,26 +1,26 @@
 package Woche3;
 
 public class aufgabeEins {
-    static double meine_berechnung(final double basis, final int exponent) {
+    static double meineBerechnung(final double BASIS, final int EXPONENT) {
         double result = 1;
-        if (exponent < 0) {
-            int anzahl = exponent * -1;
+        if (EXPONENT < 0) {
+            int anzahl = EXPONENT * -1;
             while (anzahl > 0) {
-                result = result / basis;
+                result = result / BASIS;
                 anzahl -= 1;
             }
             return result;
         } else {
-            int Count = exponent;
-            while (Count > 0) {
-                result = result * basis;
-                Count -= 1;
+            int count = EXPONENT;
+            while (count > 0) {
+                result = result * BASIS;
+                count -= 1;
             }
             return result;
         }
     }
     
     public static void main(String[] args) {
-        System.out.println(meine_berechnung(5.0, 2));
+        System.out.println(meineBerechnung(5.0, 2));
     }
 }
