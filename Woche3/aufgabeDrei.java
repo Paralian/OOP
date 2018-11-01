68f965c578c8e500f59a04d2e2fdf6c69e300f63
Paralian <eyeize@gmail.com>
2018-11-01 18:42:22 +0100

    A3 aufgeräumt

--- Woche3/aufgabeDrei.java
+++ Woche3/aufgabeDrei.java
@@ -1,14 +1,13 @@
 package Woche3;
 
 public class aufgabeDrei {
-    static boolean isBetween(int val, int b1, int b2) {
-        int lo = b1;
-        int hi = b2;                        // set boundaries as they should normally be (from low to high)
-        if (b1 > b2) {                      // check if boundaries ordered incorrectly
-            hi = b1;                        // swap boundaries
-            lo = b2;
+    static boolean isBetween(int val, int lo, int hi) {
+        if (lo <= hi) {                                     // check if boundaries ordered incorrectly
+            return (lo <= val && val <= hi);                // compare lower bound with value AND compare higher bound with value
+        } else {
+            return (hi <= val && val <= lo);                // if boundaries incorrect, swap boundaries & compare
         }
-        return (lo <= val && val <= hi);    // compare lower bound with value AND compare higher bound with value
+        
     }
     
     /**
@@ -26,8 +25,8 @@ public class aufgabeDrei {
         int price = 0;                                                           //  Preis (Cent)
         
         //  M[i][const]: unterschiedliche Preisklassen
-        //  M[const][j]: price, weight(lo, hi), height(lo, hi), width(lo, hi), length(lo, hi)
-        final int[][] M = {{45, 0, 2, 0, 15, 90, 125, 140, 235},                       //  Postkarte
+        //  M[const][j]: price, height(lo, hi), weight(lo, hi), width(lo, hi), length(lo, hi)
+        final int[][] M = {{45, 0, 2, 0, 15, 90, 125, 140, 235},                 //  Postkarte
                 {70, 0, 5, 0, 20, 90, 125, 140, 235},                            //  Standard
                 {85, 0, 10, 0, 50, 70, 125, 100, 235},                           //  Kompakt
                 {145, 0, 20, 0, 500, 70, 250, 100, 353},                         //  Gross
@@ -39,23 +38,22 @@ public class aufgabeDrei {
         /*  vergleiche Paketabmessungen mit den Vorgaben */
         int pass = 0;                                                            // interner Zähler von passenden Parametern
         
-        spalte:
+        check:
         for (int i = 0; i < M.length; i++) {                                     // iteriere m[][] Zeilenweise
-            zeile:
+            line:
             for (int j = 1; j < M[i].length - 1; j = j + 2) {                    // iteriere m[][] innerhalb der Zeile, Spaltenweise(2x)
                 if (isBetween(p[((j + 1) / 2) - 1], M[i][j], M[i][j + 1])) {     // verknüpfe Pointer von p[] mit j aus m[][]
                     pass++;                                                      // und prüfe, ob Betrag P[x] im Intervall M
                     if (pass >= 4) {                                             // falls ja, inkrementiere PASS
                         price = M[i][0];                                         // und prüfe ob alle Abmessungen gestimmt haben
-                        break spalte;                                            // wenn alles stimmt, nehme Preis
+                        break check;                                             // wenn alles stimmt, nehme Preis
                     }                                                            // aus der ersten Spalte in M
                 } else {                                                         // sonst beim ersten nehme nächste Zeile aus m[][]
                     pass = 0;                                                    // und setze PASS zurück
-                    break zeile;
+                    break line;
                 }
             }
         }
-        
         return price;
     }
     
@@ -65,6 +63,5 @@ public class aufgabeDrei {
         int height = 31;
         int weight = 415;
         System.out.println(postage(length, width, height, weight));
-        
     }
 }
