58c5abca0249b42c50e74cd2857824063385ed77
mnpqraven <mnpq.raven@gmail.com>
2018-11-02 15:08:50 +0100

    update gitignore

--- Woche3/aufgabeDrei.java
+++ Woche3/aufgabeDrei.java
@@ -1,43 +1,4 @@
 public class aufgabeDrei {
-<<<<<<< HEAD
-    public static boolean isBetween(int value, int lower, int upper) {
-        if (value <= upper && value >= lower) {
-            return true;
-        } else return false;
-    }
-    //isBetween(length,140,235))
-    public static int postage(int length, int width, int height, int weight) {
-        if (isBetween(length,140,235)) {
-            if (isBetween(width,90,125)){
-                if (isBetween(height,0,2)){
-                    if (isBetween(weight,0,15))
-                        return 45;
-                }
-                if (isBetween(height,0,5)){
-                    if (isBetween(weight,0,20))
-                        return 70;
-                }
-            }
-        }
-        if (isBetween(length,100,235)){
-            if (isBetween(width,70,125)) {
-                if (isBetween(height,0,10)){
-                    if (isBetween(weight,0,50))
-                        return 85;
-                }
-            }
-        }
-        else if (isBetween(length,100,353)) {//gross + maxi
-            if (isBetween(width,70,250)) {
-                if (isBetween(height,0,20)){//gross
-                    if (isBetween(weight,0,500))
-                        return 145;
-                }
-                if (isBetween(height,0,50)){//maxi
-                    if (isBetween(weight,0,1000))
-                        return 260;
-                }
-=======
     static boolean isBetween(int value, int lower, int upper) {
         return (value <= upper) && (lower <= value);
     }
@@ -60,26 +21,17 @@ public class aufgabeDrei {
             }
             if (isBetween(height, 0, 50) && isBetween(weight, 0, 1000)) {
                 return 260;
->>>>>>> KIEN
             }
         }
         return 0;
     }
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println(postage(180,100,2,2));//stufe1
-        System.out.println(postage(180,100,4,17));//stufe2
-        System.out.println(postage(110,80,2,2));//stufe3
-        System.out.println(postage(300,80,2,2));//stufe4
-        System.out.println(postage(300,80,2,800));//stufe5
-=======
         System.out.println("Portokosten für Postkarte:" + postage(150, 100, 1, 10));
         System.out.println("Portokosten für Standard:" + postage(150, 100, 3, 18));
         System.out.println("Portokosten für Kompakt:" + postage(150, 100, 6, 30));
         System.out.println("Portokosten für Groß:" + postage(150, 100, 15, 100));
         System.out.println("Portokosten für Maxi:" + postage(150, 100, 20, 800));
         System.out.println("Nicht identifizierbar:" + postage(150, 100, 1, 1100));
->>>>>>> KIEN
     }
 }
--- Woche3/aufgabeZwei.java
+++ Woche3/aufgabeZwei.java
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-package Woche3;
-public class aufgabeZwei {
-    public static double pStart ;//root of 12
-    public static int n; //step
-    public static double pNext;
-//TODO: debug
-    static double piIterative(int n) {
-        while (n >= 0){
-            pStart = Math.sqrt(12);
-            pNext = Math.sqrt(12)*((Math.pow((-1.0/3.0),n))/(2*n+1)) + pStart;
-            pStart = pNext;
-            n--;
-        }
-        return pNext;
-    }
-
-    static double piRecursive(int n) {
-        pStart = Math.sqrt(12);
-        pNext = Math.sqrt(12)*((Math.pow((-1.0/3.0),n))/(2*n+1)) + pStart;
-        pStart = pNext;
-        n--;
-        if (n >= 0) piRecursive(n);
-        return pNext;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(piIterative(0));
-        System.out.println(piIterative(3));
-        System.out.println(piRecursive(3));
-        System.out.println(piRecursive(6));
-=======
 public class aufgabeZwei {
     /**
      * This method approximates the value of PI with a while-loop after n steps.
@@ -63,6 +31,5 @@ public class aufgabeZwei {
         System.out.println("result of piIterative with n = 15 is " + piIterative( 15));
         System.out.println("result of piRecursive with n = 5 is " + piRecursive( 5));
         System.out.println("result of piRecursive with n = 15 is " + piRecursive( 15));
->>>>>>> KIEN
     }
 }
