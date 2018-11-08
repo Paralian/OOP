e61cb3939540d3cc3bff3bc18e80471ea2b81096
Othi <mnpq.raven@gmail.com>
2018-11-08 15:13:31 +0100

    abcd done + tested, (un)solve() recursion left

--- Woche4/aufgabeZwei.java
+++ Woche4/aufgabeZwei.java
@@ -4,7 +4,7 @@ public class aufgabeZwei {
      * 1. 1st ring can always be removed/added
      * 2. if n-2 is removed and n-1 is present then n can be added/removed
      * 3. 1 ring per move
-     * n-2(0) n-1(1) = n.canMove;
+     * n-2 -> 0(1) && n-1(0) = n.canMove;
      */
     int iCount = 8;
 
@@ -20,16 +20,26 @@ public class aufgabeZwei {
         return false;
     }
 
-    static boolean canMove(byte stick, int pos){
-        if (isOnStick(stick,pos-2) && !isOnStick(stick,pos-1) || pos == 0)
+
+    static boolean canMove(byte stick, int pos) {
+        if (pos == 0)
             return true;
+        if (pos == 1 && isOnStick(stick,0))
+            return true;
+        if (pos>= 2 && isOnStick(stick,pos-1)){
+            for (int i = 0; i < pos-1; i++) {
+                if (isOnStick(stick,i))
+                    return false;
+            }
+            return true;
+        }
         return false;
     }
 
     static byte move(byte stick, int pos, boolean on){
         if (canMove(stick, pos)){
             if (on) {//true, 1 -> 0, unset the bit
-                stick |= (byte) (stick & ~(1 << pos));
+                stick = (byte) (stick & ~(1 << pos));
                 return stick;
             }
             else {//false, 0 -> 1, set the bit
@@ -44,18 +54,32 @@ public class aufgabeZwei {
         System.out.println(String.format("%8s", Integer.toBinaryString(stick & 0xFF)));
     }
 
-    static byte solve(byte stick, int rings){
-        //TODO:
+    static byte solve(byte stick, int rings){//all to 1
+        //TODO:make pos.canMove -> rings recursive
+        //TODO: scan canMove() after every move()
+        for (int i = 0; i <= rings; i++) {
+            if (isOnStick(stick,i)) {
+                stick = move(stick, i, false);
+                printStick(stick);
+            }
+            else {
+                stick = move(stick,i,true);
+                printStick(stick);
+            }
+
+        }
+        return stick;
     }
     /*
         byte unsolve(byte stick, int rings);
         */
     public static void main(String[] args) {
+        //TODO: test console cleanup
         /**
          * BYTE STRING CONVERSION TEST CONSOLE
          */
-        byte bTest = (byte) 200;
-        int iPos = 0;
+        byte bTest = (byte) 23;
+        int iPos = 4;
 
         int res1 = bTest >> iPos;
         int res2 = bTest >> iPos & 1;
@@ -72,16 +96,21 @@ public class aufgabeZwei {
         }
 
         /**
+         * canMove() TEST CONSOLE
+         */
+        byte moveTest = (byte) 191;
+        printStick(moveTest);
+        System.out.println("canMove " + canMove(moveTest,7));
+
+        /**
          * isOnStick(), canMove(), move() TEST CONSOLE
          */
-        byte bTest2 = (byte) -27;
-        int iPos2 = 3;
-        boolean on = false;
-        for (int i = 0; i < 8; i++) {
-            System.out.println(canMove(bTest2,i));
-        }
+        byte bTest2 = (byte) 23;
+        int iPos2 = 4;
+        boolean on = true;
+
         String s4= String.format("%8s", Integer.toBinaryString(bTest2 & 0xFF)).replace(' ', '0');
-        System.out.println(s4 + " is on stick: " +isOnStick(bTest,iPos2)
+        System.out.println(s4 + " pos " +iPos2+" is on stick: " +isOnStick(bTest,iPos2)
                 +" can move: "+ canMove(bTest2,iPos2));
         byte res3 = move(bTest2,iPos2,on);
         System.out.println("res3: "+res3);
@@ -92,9 +121,11 @@ public class aufgabeZwei {
          * printStick() TEST CONSOLE
          */
         printStick(bTest2);
-
+        printStick(res3);
         /**
          * solve() AND unsolve() TEST CONSOLE
          */
+        System.out.println("solve() TEST with " + s4);
+        solve(bTest2,7);
     }
 }
