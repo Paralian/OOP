5b785dcb7d0bf2159904cc24bbcd831fcfc1f5a4
Othi <mnpq.raven@gmail.com>
2018-11-08 13:40:06 +0100

    2bcd done + tested, solve and unsolve() left

--- Woche4/aufgabeZwei.java
+++ Woche4/aufgabeZwei.java
@@ -4,7 +4,7 @@ public class aufgabeZwei {
      * 1. 1st ring can always be removed/added
      * 2. if n-2 is removed and n-1 is present then n can be added/removed
      * 3. 1 ring per move
-     * n-2(1) n-1(0) = n.canMove;
+     * n-2(0) n-1(1) = n.canMove;
      */
     int iCount = 8;
 
@@ -21,22 +21,42 @@ public class aufgabeZwei {
     }
 
     static boolean canMove(byte stick, int pos){
-        if (!isOnStick(stick,pos-2) && isOnStick(stick,pos-1) || pos == 0)
+        if (isOnStick(stick,pos-2) && !isOnStick(stick,pos-1) || pos == 0)
             return true;
         return false;
     }
 
+    static byte move(byte stick, int pos, boolean on){
+        if (canMove(stick, pos)){
+            if (on) {//true, 1 -> 0, unset the bit
+                stick |= (byte) (stick & ~(1 << pos));
+                return stick;
+            }
+            else {//false, 0 -> 1, set the bit
+                stick = (byte) (stick | (1 << pos));
+                return stick;
+            }
+        }
+        return stick;
+    }
+
+    static void printStick(byte stick){
+        System.out.println(String.format("%8s", Integer.toBinaryString(stick & 0xFF)));
+    }
+
+    static byte solve(byte stick, int rings){
+        //TODO:
+    }
     /*
-        byte move(byte stick, int pos, boolean on);
-        void printStick(byte stick);
-        byte solve(byte stick, int rings);
         byte unsolve(byte stick, int rings);
         */
     public static void main(String[] args) {
-        //Test console
+        /**
+         * BYTE STRING CONVERSION TEST CONSOLE
+         */
         byte bTest = (byte) 200;
         int iPos = 0;
-        //
+
         int res1 = bTest >> iPos;
         int res2 = bTest >> iPos & 1;
 
@@ -50,5 +70,31 @@ public class aufgabeZwei {
         for (int i = 0; i < 8; i++) {
             System.out.println("POS " + i + " "+ isOnStick(bTest,i));
         }
+
+        /**
+         * isOnStick(), canMove(), move() TEST CONSOLE
+         */
+        byte bTest2 = (byte) -27;
+        int iPos2 = 3;
+        boolean on = false;
+        for (int i = 0; i < 8; i++) {
+            System.out.println(canMove(bTest2,i));
+        }
+        String s4= String.format("%8s", Integer.toBinaryString(bTest2 & 0xFF)).replace(' ', '0');
+        System.out.println(s4 + " is on stick: " +isOnStick(bTest,iPos2)
+                +" can move: "+ canMove(bTest2,iPos2));
+        byte res3 = move(bTest2,iPos2,on);
+        System.out.println("res3: "+res3);
+        String s5 = String.format("%8s", Integer.toBinaryString(res3 & 0xFF)).replace(' ', '0');
+        System.out.println("moved string: "+s5);
+
+        /**
+         * printStick() TEST CONSOLE
+         */
+        printStick(bTest2);
+
+        /**
+         * solve() AND unsolve() TEST CONSOLE
+         */
     }
 }
