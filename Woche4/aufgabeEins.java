980b0e0a74f680d53f80f84d61568a952f5d0a68
Paralian <eyeize@gmail.com>
2018-11-13 22:57:27 +0100

    W4A1 cleanup&doc final W4A2 is still a mess, taking the version from Tung

--- Woche4/aufgabeEins.java
+++ Woche4/aufgabeEins.java
@@ -3,15 +3,20 @@ package Woche4;
 import static java.lang.Math.*;
 
 public class aufgabeEins {
+    /**
+     * converts a positive decimal integer into a simple binary number
+     * @param n decimal integer to convert
+     * @return  converted binary number
+     */
     static String toBinary(int n) {
         final int BASE = 2;
         String result = "";
         if (n >= 0) {
             int power = (int) (log(n) / log(BASE));             //  get max power of 2 so that pow(2, power) <= n
             while (power >= 0 && n >= 0) {
-                if (n - pow(BASE, power) >= 0) {
+                if (n - pow(BASE, power) >= 0) {                //  check if n larger than current set power of 2
                     result = result + "1";                      //  subtraction positive -> top bit = 1
-                    n = n - ((int) pow(BASE, power));           //  subtract
+                    n = n - ((int) pow(BASE, power));           //  update n
                     power--;
                 } else {
                     result = result + "0";                      //  subtraction not positive -> top bit = 0
@@ -19,11 +24,16 @@ public class aufgabeEins {
                 }
             }
         } else {
-            result = "please use toTwosComplement";
+            return "please use toTwosComplement";
         }
         return result;
     }
     
+    /**
+     * converts a decimal integer into a number in base 8
+     * @param n decimal integer to convert
+     * @return converted octal number
+     */
     static String toOctal(int n) {
         final int BASE = 8;
         String result = "";
@@ -45,6 +55,11 @@ public class aufgabeEins {
         return result;                          // *thinking* this may work for any base if you take it from method signature
     }
     
+    /**
+     * converts a decimal integer into a binary number in two's complement with 8 bits total
+     * @param n decimal integer to convert
+     * @return converted binary number in two's complement
+     */
     static String toTwosComplement(byte n) {
         String result;
         final int BASE = 2;
@@ -69,15 +84,12 @@ public class aufgabeEins {
     }
     
     //  BONUS: number n in b's base, works for base 2-16
-    
     /**
-     * converts number n into base b
-     *
-     * @param n number for conversion
+     * converts a decimal integer into any base 2-16
+     * @param n decimal integer for conversion
      * @param b base of conversion
-     * @return number n in base b
+     * @return converted number n in base b
      */
-    
     
     static String toBaseX(int n, int b) {
         if (b < 2 || b > 16) {
@@ -86,22 +98,21 @@ public class aufgabeEins {
         final int BASE = b;
         String result = "";
         if (n == 0) {
-            return "0";
+            return "0";             // just simplifying the method for case zero
         } else if (n < 0) {
             n = n * -1;
             result = "-";
         }
         int power = (int) (log(n) / log(BASE));             //  get max power of BASE so that pow(BASE, power) <= n
-        
+        char c[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'}; // base digits
         while (power >= 0 && n >= 0) {
-            char c[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
             int i = 0;
             while (n - pow(BASE, power) >= 0) {
-                i++;                                        //  count how many BASE's there are
-                n = n - ((int) pow(BASE, power));
+                i++;                                        //  count how many BASE's there are in n
+                n = n - ((int) pow(BASE, power));           //  update n
             }
-            power--;
-            result = result + c[i];                            //  record the first number, go on for second etc.
+            power--;                                        //  go to lower power
+            result = result + c[i];                         //  record that first number, go on for the next etc.
         }
         
         return result;
@@ -115,7 +126,7 @@ public class aufgabeEins {
         byte d = 5;
         
         
-        System.out.println("-42 in BaseX where X is 3: " + toBaseX(-42, 3));
+        
         System.out.println(a + " in BIN1C is: " + toBinary(a));
         System.out.println("-- Why do mathematicians have trouble holding Halloween and Christmas apart?\n" +
                 "-- Because OCT" + toOctal(b) + " = DEC25");
@@ -124,5 +135,6 @@ public class aufgabeEins {
         System.out.println(d + " in BIN2C: " + toTwosComplement(d));
         System.out.println(n + " in BIN2C: " + toTwosComplement((byte) n));
         System.out.println("For comparison with toBinaryString() result: " + Integer.toBinaryString((n & 0xFF) + 256).substring(1));
+        System.out.println("-42 in BaseX where X is 16: " + toBaseX(-42, 16));
     }
 }
--- Woche4/aufgabeZwei.java
+++ Woche4/aufgabeZwei.java
@@ -1,75 +1,81 @@
 package Woche4;
 
 import static java.lang.Integer.toBinaryString;
-import static java.lang.Math.*;
 
 public class aufgabeZwei {
+    /**
+     * checks if ring at current position is on the stick
+     * @param stick the playing stick in current state
+     * @param pos position of the pointer, usage: (1<<--pos)
+     * @return TRUE if ring is on stick, otherwise FALSE
+     */
     static boolean isOnStick(byte stick, int pos) {      // pos = [1,8]
-        return ((~stick & (1 << (pos - 1))) == (1 << (pos - 1)));             // invert stick, pass through AND-mask of position
+        return ((~stick & (1 << --pos)) == (1 << --pos));             // invert stick, pass through AND-mask of position
     }
     
+    /**
+     * checks if current position is OK for putting a ring on it or taking it off
+     * @param stick the playing stick in current state
+     * @param pos position of the pointer
+     * @return TRUE if current position is suitable for re/moving a ring, FALSE otherwise
+     */
     static boolean canMove(byte stick, int pos) {               // pos movable if pos-1 on and pos-2 to 0 off
         return (pos == 1) || ((isOnStick(stick, pos - 1)) && (((~stick >>> pos - 2) << pos - 2) == ~stick));     // ~stick & mask == mask?
     }
     
+    /**
+     * moves the ring at the set position on or off the ring
+     * @param stick the playing stick in current state
+     * @param pos position of the pointer, usage: (1<<--pos)
+     * @param on TRUE for putting ring on, FALSE for taking ring off
+     * @return stick with new ring or old ring removed
+     */
     static byte move(byte stick, int pos, boolean on) {
-        if (canMove(stick, pos)) {
-            if (isOnStick(stick, pos) && !on) {
-                stick = (byte) ~(~stick & ~(1 << (pos - 1)));     // ring on stick & movable -> take ring off
+        if (canMove(stick, pos)) {                              // check if movable
+            if (on) {
+                stick = (byte) (stick & ~(1 << --pos));         // ring on & removable -> take ring off
             } else {
-                stick = (byte) (stick | (1 << (pos - 1)));       // ring not on stick, stick ringable -> put ring on
+                stick = (byte) (stick | (1 << --pos));          // ring off & can be added -> put ring on
             }
         }
         return stick;
     }
     
+    /**
+     * prints the current state of stick on console
+     * @param stick the stick for printing
+     */
     static void printStick(byte stick) {
         System.out.println(toBinaryString((stick & 0xFF) + 256).substring(1));
     }
     
+    /**
+     * solves the 00000000 stick into xxxx1111 stick
+     * @param stick the starting stick
+     * @param rings number of rings to be taken off of the stick at n rightmost positions
+     * @return solved stick with n rightmost rings off
+     */
     static byte solve(byte stick, int rings) {
         if (rings == 1) {
-            stick = move(stick, 1, false);
-            printStick(stick);
-            return stick;
+        
         }
         if (rings == 2) {
-            stick = move(stick, 2, false);
-            printStick(stick);
-            stick = solve(stick, 1);
-            return stick;
-        } else {
-            stick = solve(stick, rings - 2);
-            stick = move(stick, rings, false);
-            printStick(stick);
-            stick = unsolve(stick, rings - 2);
-            stick = solve(stick, rings - 1);
-            return stick;
+        
+        }
+        else {
+            solve(unsolve(stick, rings-2), rings-1);
+            move(stick, rings, false);
         }
     }
     
     static byte unsolve(byte stick, int rings) {
-        if (rings == 1) {
-            stick = move(stick, 1, true);
-            printStick(stick);
-            return stick;
-        }
-        if (rings == 2) {
-            stick = unsolve(stick, 1);
-            stick = move(stick, 2, true);
-            printStick(stick);
-            return stick;
-        } else {
-            stick = unsolve(stick, rings - 1);
-            stick = solve(stick, rings - 2);
-            stick = move(stick, rings, true);
-            printStick(stick);
-            stick = unsolve(stick, rings - 2);
-            return stick;
-        }
+        return stick;
     }
     
+    
     public static void main(String[] args) {
-        solve((byte) 0, 3);
+        solve((byte) 0, 8);
+        System.out.println("\n");
+        
     }
 }
