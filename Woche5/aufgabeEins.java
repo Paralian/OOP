49d4a855bec2eef1c00fedc21540051246cef447
Paralian <eyeize@gmail.com>
2018-11-15 21:17:11 +0100

    W4A1 minor cleanup of getMinimum, rework of getMaximum, debug of calculateAverage, cleanup of isSorted

--- Woche5/aufgabeEins.java
+++ Woche5/aufgabeEins.java
@@ -1,9 +1,9 @@
 package Woche5;
 
-import java.util.Arrays;
+import static java.util.Arrays.copyOf;
 
 public class aufgabeEins {
-
+    
     /**
      * finds the minimal value in a float array
      *
@@ -12,14 +12,14 @@ public class aufgabeEins {
      */
     static float getMinimum(float[] numbers) {
         float result = numbers[0];
-        for (int i = 0; i < numbers.length; i++) {
-            if (result >= numbers[i]) {
+        for (int i = 1; i < numbers.length - 1; i++) {
+            if (result > numbers[i]) {
                 result = numbers[i];
             }
         }
         return result;
     }
-
+    
     /**
      * finds the maximal value in a float array
      *
@@ -27,27 +27,32 @@ public class aufgabeEins {
      * @return maximum of this array
      */
     static float getMaximum(float[] numbers) {
-        if (numbers.length <= 2) {
-            return (numbers[0] >= numbers[numbers.length - 1] ? numbers[0] : numbers[numbers.length - 1]);
-        } else return (getMaximum(Arrays.copyOfRange(numbers, 0, numbers.length - 1)));
+        float maximum = (numbers[0] > numbers[numbers.length - 1] ? numbers[0] : numbers[numbers.length - 1]);
+        ;
+        while (numbers.length > 2) {
+            numbers = copyOf(numbers, numbers.length - 1);
+            maximum = (getMaximum(numbers) > maximum ? getMaximum(numbers) : maximum);
+            return maximum;
+        }
+        return maximum;
     }
-
+    
     /**
      * calculates the average value of a float array
      *
      * @param numbers this array
      * @return arithmetic average of this array
      */
+    
     static float calculateAverage(float[] numbers) {
-        float result = 0;
-        for (float number :
-                numbers) {
-            result = result + (number / numbers.length);
+        float sum = 0;
+        for (float number : numbers) {
+            sum = sum + number;
         }
-        return result;
-
+        return sum / numbers.length;
+        
     }
-
+    
     /**
      * checks if this float array is sorted, ascending or descending
      *
@@ -59,13 +64,13 @@ public class aufgabeEins {
         int i = 0;
         boolean result = false;
         if (ascending) {
-            while (i <= numbers.length - 2) {
+            while (i < numbers.length - 1) {
                 result = (numbers[i] <= numbers[i + 1]);
                 if (!result) break;
                 i++;
             }
         } else {
-            while (i <= numbers.length - 2) {
+            while (i < numbers.length - 1) {
                 result = (numbers[i] >= numbers[i + 1]);
                 if (!result) break;
                 i++;
@@ -73,16 +78,19 @@ public class aufgabeEins {
         }
         return result;
     }
-
+    
     public static void main(String[] args) {
         float[] num1sorted = {1, 2, 3, 4, 5};
-        float[] num1unsorted = {1, 2, 3, 5, 4};
+        float[] num1unsorted = {1, 2, 20, 5, 4};
         float[] num2sorted = {6, 4, 3, 2, -1};
         float[] num2unsorted = {7, 3, 4, 0};
         float[] ident = {0, 0, 0, 0, 0, 0};
-
+        
         System.out.println(getMinimum(num1sorted));                         // 1.0
+        System.out.println(getMaximum(num1sorted));                         // 5.0
         System.out.println(getMaximum(num2sorted));                         // 6.0
+        System.out.println(getMaximum(num1unsorted));                       // 20.0
+        System.out.println(getMaximum(ident));                              // 0.0
         System.out.println(calculateAverage(num2sorted));                   // 2.8
         System.out.println(isSorted(num1sorted, true));            // true
         System.out.println(isSorted(num2sorted, false));           // true
